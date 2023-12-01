eb1f6b3806d6d4698e2aa98c81b9acef92848ea9
NguyenTTung <cacppkk2211@gmail.com>
2023-12-01 17:55:16 +0700

    giao diện trang chủ

---
+++ WEB_520_Dynamic/Controllers/NguoiDungController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_520_Dynamic.DataAccess.Data;
+using WEB_520_Dynamic.Model;
+
+namespace WEB_520_Dynamic.Controllers
+{
+	public class NguoiDungController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public NguoiDungController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<NGUOI_DUNG> obj = _db.NGUOI_DUNGs.ToList();
+			return View(obj);
+		}
+	}
+}
