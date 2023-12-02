6a7f8cc88f163887655f9827c5147a2f68c54943
NguyenTTung <cacppkk2211@gmail.com>
2023-12-02 12:55:00 +0700

    1 phần trang chủ và giao. :(

---
+++ WEB_520_Dynamic/Controllers/NhaCungCapController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_520_Dynamic.DataAccess.Data;
+using WEB_520_Dynamic.Model;
+
+namespace WEB_520_Dynamic.Controllers
+{
+	public class NhaCungCapController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public NhaCungCapController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<NHA_CUNG_CAP> obj = _db.NHA_CUNG_CAPs.ToList();
+			return View(obj);
+		}
+	}
+}
