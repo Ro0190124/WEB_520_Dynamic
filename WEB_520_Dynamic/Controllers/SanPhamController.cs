76786ca42eb8d659e1dd2d48c57fa231aa59b0bd
NguyenTTung <cacppkk2211@gmail.com>
2023-12-04 20:47:22 +0700

    Úp tạm giao diện sp và thêm sp
    hi

---
+++ WEB_520_Dynamic/Controllers/SanPhamController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_520_Dynamic.DataAccess.Data;
+using WEB_520_Dynamic.Model;
+
+namespace WEB_520_Dynamic.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SanPhamController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<SAN_PHAM> sanPham = _db.SAN_PHAMs.ToList();
+
+            return View(sanPham);
+        }
+		public IActionResult ThemSanPham()
+		{
+			return View();
+		}
+	}
+}
