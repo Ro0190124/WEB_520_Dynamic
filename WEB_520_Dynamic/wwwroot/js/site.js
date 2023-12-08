8402a70627378f70324e38bf2340066839bdc3fa
nhunhu312 <108985946+nhunhu312@users.noreply.github.com>
2023-12-08 09:04:53 +0700

    .

--- WEB_520_Dynamic/wwwroot/js/site.js
+++ WEB_520_Dynamic/wwwroot/js/site.js
@@ -1,29 +1,4 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    // Tìm nút "Thêm người dùng" và gán sự kiện click
-    var btnThemNguoiDung = document.querySelector('.Them-TK');
-    btnThemNguoiDung.addEventListener('click', function () {
-        // Hiển thị overlay
-        var overlay = document.createElement('div');
-        overlay.setAttribute('class', 'overlay');
-        document.body.appendChild(overlay);
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
 
-        // Hiển thị popup-container
-        var popupContainer = document.querySelector('.popup-container');
-        popupContainer.style.display = 'block';
-
-        // Thiết lập CSS cho overlay
-        overlay.style.position = 'fixed';
-        overlay.style.width = '100%';
-        overlay.style.height = '100%';
-        overlay.style.top = '0';
-        overlay.style.left = '0';
-        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-        overlay.style.zIndex = '999';
-
-        // Đóng popup và overlay khi click vào overlay
-        overlay.addEventListener('click', function () {
-            popupContainer.style.display = 'none';
-            document.body.removeChild(overlay);
-        });
-    });
-});
+// Write your JavaScript code.
