1b270416997b14f078578dd2b6c3942e79e149fd
Ro0190124 <128989515+Ro0190124@users.noreply.github.com>
2023-12-11 17:40:36 +0700

    bien lai chi tiet

--- WEB_520_Dynamic/ViewComponents/BLCT_LOViewComponent.cs
+++ WEB_520_Dynamic/ViewComponents/BLCT_LOViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WEB_520_Dynamic.DataAccess.Data;
 using WEB_520_Dynamic.Model;
@@ -17,12 +18,22 @@ namespace WEB_520_Dynamic.ViewComponents
 		public async Task<IViewComponentResult> InvokeAsync(int? ID)
 		{
 			Console.WriteLine(ID +" MÃ nhàaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdkljfhalsjd");
-			IEnumerable<LO> groupSpL = _db.LOs.Include(x => x.SAN_PHAM).ToList();
-			if(groupSpL == null)
+			//IEnumerable<LO> groupSpL = _db.LOs.Include(x => x.SAN_PHAM).ToList();
+			IEnumerable<LO> groupSpL = _db.LOs.Join(_db.BIEN_LAI_CHI_TIETs, lo => lo.MaLo, blct => blct.MaLo, (lo, blct) => new { lo, blct }).Where(x => x.blct.MaBienLai == ID).Select(x => x.lo).Include(x => x.SAN_PHAM).ToList();
+			//LO lo_BLCT = _db.LOs.Join(_db.BIEN_LAI_CHI_TIETs, lo => lo.MaLo, blct => blct.MaLo, (lo, blct) => new { lo, blct }).Where(x => x.blct.MaBienLai == ID).Select(x => x.lo).ToList().FirstOrDefault();*/
+			//List<LO> lo_BLCT = _db.LOs.Join(_db.BIEN_LAI_CHI_TIETs, lo => lo.MaLo, blct => blct.MaLo, (lo, blct) => new { lo, blct }).Where(x => x.blct.MaBienLai == ID).Select(x => x.lo).Include(x => x.SAN_PHAM).ToList();
+			/*foreach ( var item in lo_BLCT)
+			{
+				{
+					Console.WriteLine(item.MaLo + item.LO.TenLo);
+					Console.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+				}
+			}*/
+			if (groupSpL == null)
 			{
 				return View();
 			}
-			Console.WriteLine(groupSpL.ElementAt(0).SAN_PHAM.TenSanPham);
+			//Console.WriteLine(groupSpL.ElementAt(0).SAN_PHAM.TenSanPham);
 			
 			return View(groupSpL);
 		}
